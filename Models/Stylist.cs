a1b93a001ebe64ddd7510021454d2febd3d620e7
Student <example@example.com>
2020-01-24 13:27:34 -0800

    add stylist class and controller

---
+++ Models/Stylist.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+    {
+        public Stylist()
+        {
+            this.Clients = new HashSet<Item>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
